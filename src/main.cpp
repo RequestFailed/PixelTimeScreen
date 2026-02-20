e882f17a4f8abd8b49ba387342ca885d853049e9
RequestFailed <2943475104@qq.com>
2026-02-20 16:46:22 +0800

    ver0.1.3 改正0.1.2utc卡住bug 加入网络连接动画

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 /** 
- * 删除了boom
- * 加入了UTC时间显示功能
+ * 改正了0.1.2中的utc卡住不动的bug
+ * 加入了网络连接提示动画
 */
 
 #include <Arduino.h>
@@ -25,26 +25,36 @@ Adafruit_NeoMatrix matrix = Adafruit_NeoMatrix(32, 8, PIN,
   NEO_GRB            + NEO_KHZ800);
 
 char previousTimeString[6] = {0};
-char boom[4] = {0};
-char UTC[3] = {0};
-char TIME[4] = {0};
 char UTCTimeString[6] = {0};
 
 void setup() {
+
   matrix.begin();
   matrix.setTextWrap(false);
   matrix.setBrightness(64);
-  matrix.setTextColor(matrix.Color(0xE0, 0x10, 0xFF)); // Purple
+  matrix.setTextColor(matrix.Color(0xD0, 0xD0, 0xFF)); // Light Blue
 
   WiFi.begin(wifi_ssid, wifi_password);
-  while (WiFi.status() != WL_CONNECTED) delay(500);
+
+  matrix.fillScreen(0);
+  matrix.setCursor(1, 1);
+  matrix.print("WiFi?");
+  matrix.show();
   
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+  }
+
+  matrix.fillScreen(0);
+  matrix.setCursor(1, 1);
+  matrix.setTextColor(matrix.Color(0x00, 0xFF, 0x00));
+  matrix.print("WL OK");
+  matrix.show();
+
   configTime(28800, 0, ntpServer); // 时区，夏令时，时间服务器
   // 28800 = 8小时 = 东8区
 
-  sprintf(boom, "boom");
-  sprintf(UTC, "UTC");
-  sprintf(TIME, "TIME");
+  matrix.setTextColor(matrix.Color(0xD0, 0xD0, 0xFF)); // Light Blue
 
 }
 
@@ -58,20 +68,20 @@ void loop() {
   sprintf(timeString, "%02d:%02d", timeinfo.tm_hour, timeinfo.tm_min);
   sprintf(UTCTimeString, "%02d:%02d", (timeinfo.tm_hour-8>=0?timeinfo.tm_hour-8:timeinfo.tm_hour+16), timeinfo.tm_min);
 
-  if (strcmp(previousTimeString, timeString) != 0) { // 减少不必要的刷新
-    matrix.fillScreen(0);    // 清屏
-    matrix.setCursor(1, 1);
-    if (timeinfo.tm_sec >= 0 && timeinfo.tm_sec <= 5) {
-      matrix.print(UTC);
-    } else if (timeinfo.tm_sec >= 6 && timeinfo.tm_sec <= 10) {
-      matrix.print(UTCTimeString);
-    } else {
-      matrix.print(timeString);
-    }
-    matrix.show();
-    for (int i = 0; i < 6; i++) {
-      previousTimeString[i] = timeString[i];
-    }
+  matrix.fillScreen(0);    // 清屏
+  matrix.setCursor(1, 1);
+  if (timeinfo.tm_sec >= 0 && timeinfo.tm_sec <= 3) {
+    matrix.print("UTC");
+  } else if (timeinfo.tm_sec >= 4 && timeinfo.tm_sec <= 8) {
+    matrix.print(UTCTimeString);
+  } else if (timeinfo.tm_sec >= 9 && timeinfo.tm_sec <= 11) {
+    matrix.print("Local");
+  } else {
+    matrix.print(timeString);
+  }
+  matrix.show();
+  for (int i = 0; i < 6; i++) {
+    previousTimeString[i] = timeString[i];
   }
 
   delay(1000); 
