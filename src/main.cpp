e1b99bc56130d9adc650b0d7cbf8db72d2222824
RequestFailed <2943475104@qq.com>
2026-02-20 16:24:56 +0800

    ver0.1.2 删除了boom 加入utc时间显示功能

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,6 @@
 /** 
- * 这一版的程序将wifi的ssid和密码存储在了/lib/wifi_config.cpp中，请自行创建并修改。
- * 模板(wifi_config.cpp)
- * namespace wifi_config {
- *  const char* wifi_ssid = "你的wifi名称";
- *  const char* wifi_password = "你的wifi密码";
- *  const char* ntpServer = "你的ntp服务器(建议pool.ntp.org)";
- * }
- * 
- * 另外，此程序加入了在每分钟的开始(0s-5s)区间显示单词boom的实(zhi)用(zhang)小功能~
- * ps:其实很傻并且屁用没有
+ * 删除了boom
+ * 加入了UTC时间显示功能
 */
 
 #include <Arduino.h>
@@ -32,6 +24,12 @@ Adafruit_NeoMatrix matrix = Adafruit_NeoMatrix(32, 8, PIN,
   NEO_MATRIX_COLUMNS + NEO_MATRIX_ZIGZAG,
   NEO_GRB            + NEO_KHZ800);
 
+char previousTimeString[6] = {0};
+char boom[4] = {0};
+char UTC[3] = {0};
+char TIME[4] = {0};
+char UTCTimeString[6] = {0};
+
 void setup() {
   matrix.begin();
   matrix.setTextWrap(false);
@@ -43,10 +41,12 @@ void setup() {
   
   configTime(28800, 0, ntpServer); // 时区，夏令时，时间服务器
   // 28800 = 8小时 = 东8区
-}
 
-char previousTimeString[6] = { 0 };
-char boom[4];
+  sprintf(boom, "boom");
+  sprintf(UTC, "UTC");
+  sprintf(TIME, "TIME");
+
+}
 
 void loop() {
   tm timeinfo;
@@ -56,12 +56,15 @@ void loop() {
 
   char timeString[6];
   sprintf(timeString, "%02d:%02d", timeinfo.tm_hour, timeinfo.tm_min);
-  sprintf(boom, "boom");
+  sprintf(UTCTimeString, "%02d:%02d", (timeinfo.tm_hour-8>=0?timeinfo.tm_hour-8:timeinfo.tm_hour+16), timeinfo.tm_min);
+
   if (strcmp(previousTimeString, timeString) != 0) { // 减少不必要的刷新
     matrix.fillScreen(0);    // 清屏
     matrix.setCursor(1, 1);
     if (timeinfo.tm_sec >= 0 && timeinfo.tm_sec <= 5) {
-      matrix.print(boom);
+      matrix.print(UTC);
+    } else if (timeinfo.tm_sec >= 6 && timeinfo.tm_sec <= 10) {
+      matrix.print(UTCTimeString);
     } else {
       matrix.print(timeString);
     }
